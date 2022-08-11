f8080955c7491837257c34b7e7e39e9065735dec
Trevor Royer <troyer@redhat.com>
2022-08-11 09:25:15 -0500

    reformat with black

--- services/anomaly_data_service.py
+++ services/anomaly_data_service.py
@@ -2,69 +2,74 @@ import pandas as pd
 import psycopg2
 
 from services.connection_pool_singleton import ConnectionPoolSingleton
+
+
 class AnomalyDataService:
     """Executes and returns queries from a Postgres database"""
 
-    @staticmethod 
+    @staticmethod
     def get_all_data():
-       """Returns all rows from table and reads it into a pandas dataframe"""
-       GET_ALL_ROWS_SQL = 'SELECT * FROM prediction'
-       pool = ConnectionPoolSingleton.getConnectionPool()
-       conn = pool.getconn()
-       cursor = conn.cursor()
-       try: 
-           cursor.execute(GET_ALL_ROWS_SQL)
-           anomaly = pd.read_sql_query(GET_ALL_ROWS_SQL,conn)
-       except (Exception, psycopg2.DatabaseError) as error:
+        """Returns all rows from table and reads it into a pandas dataframe"""
+        GET_ALL_ROWS_SQL = "SELECT * FROM prediction"
+        pool = ConnectionPoolSingleton.getConnectionPool()
+        conn = pool.getconn()
+        cursor = conn.cursor()
+        try:
+            cursor.execute(GET_ALL_ROWS_SQL)
+            anomaly = pd.read_sql_query(GET_ALL_ROWS_SQL, conn)
+        except (Exception, psycopg2.DatabaseError) as error:
             print(error)
-       else: 
-           return anomaly
-       finally:
-         cursor.close()
-         pool.putconn(conn)
-    
-    @staticmethod 
-    def get_sensor_25():
-       """Returns values from sensor 25 and reads them into a pandas dataframe"""
-       GET_SENSOR_25_SQL = 'SELECT sensor_25 FROM anomaly'
-       pool = ConnectionPoolSingleton.getConnectionPool()
-       conn = pool.getconn()
-       cursor = conn.cursor()
-       try:
-            cursor.execute(GET_SENSOR_25_SQL)
-            sensor_25 = pd.read_sql_query(GET_SENSOR_25_SQL,conn)
-       except (Exception, psycopg2.DatabaseError) as error:
-            print(error)
-       else:
-            return sensor_25
-       finally:
+        else:
+            return anomaly
+        finally:
             cursor.close()
             pool.putconn(conn)
-    
+
+    @staticmethod
+    def get_sensor_25():
+        """Returns values from sensor 25 and reads them into a pandas dataframe"""
+        GET_SENSOR_25_SQL = "SELECT sensor_25 FROM anomaly"
+        pool = ConnectionPoolSingleton.getConnectionPool()
+        conn = pool.getconn()
+        cursor = conn.cursor()
+        try:
+            cursor.execute(GET_SENSOR_25_SQL)
+            sensor_25 = pd.read_sql_query(GET_SENSOR_25_SQL, conn)
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        else:
+            return sensor_25
+        finally:
+            cursor.close()
+            pool.putconn(conn)
+
     @staticmethod
     def get_sensor_11():
-       """Returns values from sensor 11 and reads them into a pandas dataframe"""
-       GET_SENSOR_11_SQL = 'SELECT sensor_11 FROM anomaly'
-       pool = ConnectionPoolSingleton.getConnectionPool()
-       conn = pool.getconn()
-       cursor = conn.cursor()
-       try:
+        """Returns values from sensor 11 and reads them into a pandas dataframe"""
+        GET_SENSOR_11_SQL = "SELECT sensor_11 FROM anomaly"
+        pool = ConnectionPoolSingleton.getConnectionPool()
+        conn = pool.getconn()
+        cursor = conn.cursor()
+        try:
             cursor.execute(GET_SENSOR_11_SQL)
-            sensor_11 = pd.read_sql_query(GET_SENSOR_11_SQL,conn)
-       except (Exception, psycopg2.DatabaseError) as error:
+            sensor_11 = pd.read_sql_query(GET_SENSOR_11_SQL, conn)
+        except (Exception, psycopg2.DatabaseError) as error:
             print(error)
-       else:
+        else:
             return sensor_11
-       finally:
+        finally:
             cursor.close()
             pool.putconn(conn)
-    
-    
+
     @staticmethod
     def get_min_max(col_name, groupname):
         """Returns the minimum and maximum value of a sensor for a given value of groupname"""
-        GET_MIN_SQL = 'SELECT MIN({}) FROM anomaly WHERE groupname = {}'.format(col_name, groupname)
-        GET_MAX_SQL = 'SELECT MAX({}) FROM anomaly WHERE groupname = {}'.format(col_name, groupname)
+        GET_MIN_SQL = "SELECT MIN({}) FROM anomaly WHERE groupname = {}".format(
+            col_name, groupname
+        )
+        GET_MAX_SQL = "SELECT MAX({}) FROM anomaly WHERE groupname = {}".format(
+            col_name, groupname
+        )
         pool = ConnectionPoolSingleton().getConnectionPool()
         conn = pool.getconn()
         cursor = conn.cursor()
@@ -76,8 +81,8 @@ class AnomalyDataService:
         except (Exception, psycopg2.DatabaseError) as error:
             print(error)
         else:
-            print('The minimum value is:{}'.format(min_value[0]))
-            print('The maximum value is:{}'.format(max_value[0]))
+            print("The minimum value is:{}".format(min_value[0]))
+            print("The maximum value is:{}".format(max_value[0]))
         finally:
             cursor.close()
             pool.putconn(conn)
