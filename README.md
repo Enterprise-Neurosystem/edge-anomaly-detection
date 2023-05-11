bb4e2ac3e2474c636f6807015fd26273977bb4a0
Cory Latschkowski <koree@redhat.com>
2023-05-10 23:31:56 -0500

    update: workshop info

--- README.md
+++ README.md
@@ -3,9 +3,23 @@
 open source code for the detection of anomalies in sensor data
 This web app allows you to broadcast various sources of data in real time
 
-## OpenShift Setup
+## OpenShift Quickstart
+
+If you are following a workshop you can use the following
+
+**NOTE**: If using `Import from Git` in the OpenShift web console you will need to:
+1. Open `Advanced Git Options`
+2. Set `Context dir` to `src`
+
+```
+# set namespace to your username
+NAMESPACE=$(oc whoami)
+
+scripts/bootstrap.sh
+```
 
 You can view the default parameters for `bootstrap.sh` [here](scripts/bootstrap.sh).
+This scripted example shows you some common `oc` commands you can use to aid in deploying apps quickly.
 
 ```
 scripts/bootstrap.sh
--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -3,16 +3,21 @@
 # setup app parameters
 APP_NAME="${APP_NAME:-detect}"
 NAMESPACE="${NAMESPACE:-edge-anomaly-detection}"
-
-# other parameters
-GIT_BRANCH="main"
-APP_LABEL="app.kubernetes.io/part-of=${APP_NAME}"
 CONTEXT_DIR="src"
 
+# other parameters
+GIT_BRANCH="${GIT_BRANCH:-main}"
+APP_LABEL="app.kubernetes.io/part-of=${APP_NAME}"
+
 ocp_init(){
-oc whoami || exit 0
-# update openshift context to project
-oc project ${NAMESPACE} || oc new-project ${NAMESPACE}
+  oc whoami || exit 0
+
+  echo "NAMESPACE: ${NAMESPACE}"
+  echo "Press Ctrl + C if this is not correct"
+  sleep 5
+
+  # update openshift context to project
+  oc project ${NAMESPACE} || oc new-project ${NAMESPACE}
 }
 
 is_sourced() {
@@ -25,31 +30,32 @@ is_sourced() {
 }
 
 ocp_setup_app(){
-# setup prediction app
-oc new-app \
-  https://github.com/Enterprise-Neurosystem/edge-anomaly-detection.git#${GIT_BRANCH} \
-  --name ${APP_NAME} \
-  -l ${APP_LABEL} \
-  -n ${NAMESPACE} \
-  --context-dir ${CONTEXT_DIR}
 
-# create route
-oc expose service \
-  ${APP_NAME} \
-  -n ${NAMESPACE} \
-  -l ${APP_LABEL} \
-  --overrides='{"spec":{"tls":{"termination":"edge"}}}'
+  # setup prediction app
+  oc new-app \
+    https://github.com/Enterprise-Neurosystem/edge-anomaly-detection.git#${GIT_BRANCH} \
+    --name ${APP_NAME} \
+    -l ${APP_LABEL} \
+    -n ${NAMESPACE} \
+    --context-dir ${CONTEXT_DIR}
 
-# kludge - fix timeout for app
-oc annotate route \
-  ${APP_NAME} \
-  -n ${NAMESPACE} \
-  haproxy.router.openshift.io/timeout=5m \
-  --overwrite
+  # create route
+  oc expose service \
+    ${APP_NAME} \
+    -n ${NAMESPACE} \
+    -l ${APP_LABEL} \
+    --overrides='{"spec":{"tls":{"termination":"edge"}}}'
+
+  # kludge - fix timeout for app
+  oc annotate route \
+    ${APP_NAME} \
+    -n ${NAMESPACE} \
+    haproxy.router.openshift.io/timeout=5m \
+    --overwrite
 }
 
 main(){
-ocp_setup_app
+  ocp_setup_app
 }
 
 is_sourced || main
